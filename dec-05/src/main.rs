b3a5d07d514f35fc35cdcd605ce5eb7bb0c24293
disrupted <hi@salomonpopp.me>
2021-12-05 22:03:34 +0100

    Create test

--- dec-05/src/main.rs
+++ dec-05/src/main.rs
@@ -2,10 +2,40 @@ fn main() {
     println!("Advent of Code: Day 5");
 }
 
+fn create_diagram(input: &str) -> &str {
+    ""
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test() {}
+    fn test() {
+        let input = "
+            0,9 -> 5,9
+            8,0 -> 0,8
+            9,4 -> 3,4
+            2,2 -> 2,1
+            7,0 -> 7,4
+            6,4 -> 2,0
+            0,9 -> 2,9
+            3,4 -> 1,4
+            0,0 -> 8,8
+            5,5 -> 8,2
+        ";
+        let diagram = ".......1..
+            ..1....1..
+            ..1....1..
+            .......1..
+            .112111211
+            ..........
+            ..........
+            ..........
+            ..........
+            222111....";
+        let output = create_diagram(input);
+
+        assert!(output == diagram);
+    }
 }
