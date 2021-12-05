dcff627fc8b9781027a216babc0444f0ab9f48f6
disrupted <hi@salomonpopp.me>
2021-12-05 22:19:02 +0100

    Parse coordinates from input string

--- dec-05/src/main.rs
+++ dec-05/src/main.rs
@@ -1,8 +1,38 @@
 fn main() {
     println!("Advent of Code: Day 5");
+
+    create_diagram("");
+}
+
+fn str_to_int(s: &str) -> Result<u16, std::num::ParseIntError> {
+    s.trim().parse()
+}
+
+struct Point {
+    x: u16,
+    y: u16,
+}
+
+fn parse_coord(input: &str) -> Vec<u16> {
+    return input
+        .split(',')
+        .map(|s| str_to_int(s).unwrap())
+        .collect::<Vec<u16>>();
 }
 
 fn create_diagram(input: &str) -> &str {
+    for line in input.trim().lines() {
+        let coords = line.split(" -> ").collect::<Vec<&str>>();
+        if coords.is_empty() {
+            return "";
+        }
+
+        let start = parse_coord(coords[0]);
+        let end = parse_coord(coords[1]);
+
+        println!("start: {:?}, end: {:?}", start, end);
+    }
+
     ""
 }
 
