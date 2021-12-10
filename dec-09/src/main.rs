74473366d4ed43cf4b13dfbe3e0280c508726cac
disrupted <hi@salomonpopp.me>
2021-12-10 18:19:54 +0100

    Refactor part 1

--- dec-09/src/main.rs
+++ dec-09/src/main.rs
@@ -3,35 +3,91 @@ mod data;
 fn main() {
     println!("Advent of Code: Day 9");
 
-    let result = find_low_points(data::DATA);
-    println!("{}", result);
+    let result1 = find_low_points(data::DATA);
+    println!("result 1 {}", result1);
+}
+
+struct Floor {
+    heights: Vec<Vec<u8>>,
+}
+
+impl Floor {
+    fn height(&self) -> isize {
+        self.heights.len() as isize
+    }
+
+    fn width(&self) -> isize {
+        self.heights[0].len() as isize
+    }
+
+    fn get_height(&self, point: &Point) -> Option<u8> {
+        if point.is_within(self) {
+            return Some(self.heights[point.y as usize][point.x as usize]);
+        }
+        None
+    }
+}
+
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Point {
+    fn is_within(&self, floor: &Floor) -> bool {
+        self.x >= 0 && self.y >= 0 && self.x < floor.width() && self.y < floor.height()
+    }
+
+    fn find_adjacent(&self) -> [Point; 4] {
+        [
+            Point {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+        ]
+    }
 }
 
 fn find_low_points(data: &str) -> u32 {
-    let heights: Vec<Vec<u8>> = data
-        .trim()
-        .lines()
-        .map(|line| {
-            line.trim()
-                .chars()
-                .map(|c| c.to_digit(10).unwrap() as u8)
-                .collect()
-        })
-        .collect();
+    let floor = Floor {
+        heights: data
+            .trim()
+            .lines()
+            .map(|line| {
+                line.trim()
+                    .chars()
+                    .map(|c| c.to_digit(10).unwrap() as u8)
+                    .collect()
+            })
+            .collect(),
+    };
 
     let mut risk_level: u32 = 0;
 
-    for y in 0..heights.len() {
-        for x in 0..heights[y].len() {
-            let current = heights[y][x];
-            // check if current point is lower than surrounding points
-            if (y == 0 || current < heights[y - 1][x]) // up
-            && (x == 0 || current < heights[y][x - 1]) // left
-            && (x + 1 == heights[y].len() || current < heights[y][x + 1]) // right
-            && (y + 1 == heights.len() || current < heights[y + 1][x])
-            // down
+    for y in 0..floor.height() {
+        for x in 0..floor.width() {
+            let pos = Point { x, y };
+            let height = floor.get_height(&pos).unwrap();
+
+            if pos
+                .find_adjacent()
+                .iter()
+                .all(|adj| floor.get_height(adj).unwrap_or(u8::MAX) > height)
             {
-                risk_level += 1 + current as u32;
+                println!("{}", height);
+                risk_level += 1 + height as u32;
             }
         }
     }
