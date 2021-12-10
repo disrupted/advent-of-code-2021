e2b1c219095431dab0749b6cfdf35175483caf34
Salomon Popp <hi@salomonpopp.me>
2021-12-10 20:32:47 +0100

    day 9, puzzle 2 (#8)
    * Store digit as u8
    
    * Refactor part 1
    
    * Refactor part 1
    
    * WIP part 2
    
    * Refactor part 1 some more
    
    * WIP part 2
    
    * Solution part 2
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Run on real data

--- dec-09/src/main.rs
+++ dec-09/src/main.rs
@@ -3,48 +3,138 @@ mod data;
 fn main() {
     println!("Advent of Code: Day 9");
 
-    let result = find_low_points(data::DATA);
-    println!("{}", result);
+    let mut floor = Floor::new(data::DATA);
+    let result1 = calc_risk(&floor);
+    println!("result 1 {}", result1);
+
+    let result2 = calc_basins(&mut floor);
+    println!("result 2 {}", result2);
 }
 
-fn find_low_points(data: &str) -> u32 {
-    let heights: Vec<Vec<u32>> = data
-        .trim()
-        .lines()
-        .map(|line| {
-            line.trim()
-                .chars()
-                .map(|c| c.to_digit(10).unwrap())
-                .collect()
-        })
-        .collect();
+struct Floor {
+    heights: Vec<Vec<u8>>,
+}
 
-    println!("{:?}", heights);
-
-    let mut risk_level = 0;
-
-    for y in 0..heights.len() {
-        for x in 0..heights[y].len() {
-            let current = heights[y][x];
-            // check if current point is lower than surrounding points
-            if (y == 0 || current < heights[y - 1][x]) // up
-            && (x == 0 || current < heights[y][x - 1]) // left
-            && (x + 1 == heights[y].len() || current < heights[y][x + 1]) // right
-            && (y + 1 == heights.len() || current < heights[y + 1][x])
-            // down
-            {
-                println!("{}", current);
-                risk_level += 1 + current;
-            }
+impl Floor {
+    fn new(input: &str) -> Self {
+        Self {
+            heights: input
+                .trim()
+                .lines()
+                .map(|line| {
+                    line.trim()
+                        .chars()
+                        .map(|c| c.to_digit(10).unwrap() as u8)
+                        .collect()
+                })
+                .collect(),
         }
     }
-    risk_level
+
+    fn height(&self) -> isize {
+        self.heights.len() as isize
+    }
+
+    fn width(&self) -> isize {
+        self.heights[0].len() as isize
+    }
+
+    fn get_height(&self, point: &Point) -> Option<u8> {
+        if point.is_within(self) {
+            return Some(self.heights[point.y as usize][point.x as usize]);
+        }
+        None
+    }
+
+    fn set_visited(&mut self, point: &Point) {
+        self.heights[point.y as usize][point.x as usize] = u8::MAX;
+    }
+}
+
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Point {
+    fn is_within(&self, floor: &Floor) -> bool {
+        self.x >= 0 && self.y >= 0 && self.x < floor.width() && self.y < floor.height()
+    }
+
+    fn find_adjacent(&self) -> [Point; 4] {
+        [
+            Point {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+        ]
+    }
+}
+
+fn find_low_points(floor: &Floor) -> Vec<Point> {
+    let mut low_points = Vec::new();
+
+    for y in 0..floor.height() {
+        for x in 0..floor.width() {
+            let pos = Point { x, y };
+            let height = floor.get_height(&pos).unwrap();
+
+            pos.find_adjacent()
+                .iter()
+                .all(|adj| floor.get_height(adj).unwrap_or(u8::MAX) > height)
+                .then(|| low_points.push(pos));
+        }
+    }
+    low_points
+}
+
+fn calc_risk(floor: &Floor) -> u32 {
+    find_low_points(floor)
+        .iter()
+        .map(|p| floor.get_height(p).unwrap() as u32 + 1)
+        .sum()
+}
+
+fn calc_basins(floor: &mut Floor) -> u32 {
+    let mut basins = find_low_points(floor)
+        .iter()
+        .map(|pos| calc_basin_size(floor, pos))
+        .collect::<Vec<u32>>();
+
+    basins.sort_by(|a, b| b.cmp(a));
+    basins.iter().take(3).product()
+}
+
+fn calc_basin_size(floor: &mut Floor, pos: &Point) -> u32 {
+    floor.set_visited(pos);
+    let mut basin_size = 1;
+
+    pos.find_adjacent().iter().for_each(|adj| {
+        if floor.get_height(adj).unwrap_or(u8::MAX) < 9 {
+            basin_size += calc_basin_size(floor, adj);
+        }
+    });
+
+    basin_size
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // PART 1
     #[test]
     fn test_find_low_points() {
         const TEST_DATA: &str = "
@@ -54,6 +144,21 @@ mod tests {
             8767896789
             9899965678
         ";
-        assert_eq!(find_low_points(TEST_DATA), 15);
+        let floor = Floor::new(TEST_DATA);
+        assert_eq!(calc_risk(&floor), 15);
+    }
+
+    // PART 2
+    #[test]
+    fn test_basins() {
+        const TEST_DATA: &str = "
+            2199943210
+            3987894921
+            9856789892
+            8767896789
+            9899965678
+        ";
+        let mut floor = Floor::new(TEST_DATA);
+        assert_eq!(calc_basins(&mut floor), 1134);
     }
 }
