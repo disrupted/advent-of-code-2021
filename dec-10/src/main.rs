d41e6165ff4f389f62f959afc0e1b7e538e9473e
disrupted <hi@salomonpopp.me>
2021-12-10 13:55:49 +0100

    Calculate score

--- dec-10/src/main.rs
+++ dec-10/src/main.rs
@@ -7,6 +7,14 @@ fn main() {
     // println!("{}", result);
 }
 
+fn solve_1(data: &str) -> u32 {
+    data.trim()
+        .lines()
+        .map(find_illegal_char)
+        .map(calculate_score)
+        .sum()
+}
+
 fn get_matching_paren(c: char) -> Option<char> {
     match c {
         ')' => Some('('),
@@ -18,19 +26,37 @@ fn get_matching_paren(c: char) -> Option<char> {
 }
 
 fn is_matching_paren(s: &str) -> bool {
+    let char = find_illegal_char(s);
+    if char.is_none() {
+        return true;
+    }
+    false
+}
+
+fn find_illegal_char(s: &str) -> Option<char> {
     let mut stack: Vec<char> = vec![];
 
     for c in s.chars() {
         match c {
             ')' | ']' | '}' | '>' => {
                 if stack.pop() != get_matching_paren(c) {
-                    return false;
+                    return Some(c);
                 }
             }
             _ => stack.push(c),
         };
     }
-    true
+    None
+}
+
+fn calculate_score(c: Option<char>) -> u32 {
+    match c {
+        Some(')') => 3,
+        Some(']') => 57,
+        Some('}') => 1197,
+        Some('>') => 25137,
+        _ => 0,
+    }
 }
 
 #[cfg(test)]
@@ -42,5 +68,22 @@ mod tests {
         assert!(is_matching_paren("()"));
         assert!(is_matching_paren("([{<>}])"));
         assert!(!is_matching_paren("(]"));
+    }
+
+    #[test]
+    fn test_solve_1() {
+        const TEST_DATA: &str = "
+            [({(<(())[]>[[{[]{<()<>>
+            [(()[<>])]({[<{<<[]>>(
+            {([(<{}[<>[]}>{[]{[(<()>
+            (((({<>}<{<{<>}{[]{[]{}
+            [[<[([]))<([[{}[[()]]]
+            [{[{({}]{}}([{[{{{}}([]
+            {<[[]]>}<{[{[{[]{()[[[]
+            [<(<(<(<{}))><([]([]()
+            <{([([[(<>()){}]>(<<{{
+            <{([{{}}[<[[[<>{}]]]>[]]
+        ";
+        assert_eq!(solve(TEST_DATA), 26397);
     }
 }
