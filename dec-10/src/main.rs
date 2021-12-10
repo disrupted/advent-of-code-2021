da431beae82bfd671f208cd481f817456a5652a1
Salomon Popp <hi@salomonpopp.me>
2021-12-10 13:58:46 +0100

    day 10, puzzle 1 (#6)
    * Init day 10
    
    * Calculate score
    
    * Run on real data

---
+++ dec-10/src/main.rs
@@ -0,0 +1,89 @@
+mod data;
+
+fn main() {
+    println!("Advent of Code: Day 10");
+
+    let result1 = solve1(data::DATA);
+    println!("result 1: {}", result1);
+}
+
+fn solve1(data: &str) -> u32 {
+    data.trim()
+        .lines()
+        .map(find_illegal_char)
+        .map(calculate_score)
+        .sum()
+}
+
+fn get_matching_paren(c: char) -> Option<char> {
+    match c {
+        ')' => Some('('),
+        ']' => Some('['),
+        '}' => Some('{'),
+        '>' => Some('<'),
+        _ => None,
+    }
+}
+
+fn is_matching_paren(s: &str) -> bool {
+    let char = find_illegal_char(s);
+    if char.is_none() {
+        return true;
+    }
+    false
+}
+
+fn find_illegal_char(s: &str) -> Option<char> {
+    let mut stack: Vec<char> = vec![];
+
+    for c in s.chars() {
+        match c {
+            ')' | ']' | '}' | '>' => {
+                if stack.pop() != get_matching_paren(c) {
+                    return Some(c);
+                }
+            }
+            _ => stack.push(c),
+        };
+    }
+    None
+}
+
+fn calculate_score(c: Option<char>) -> u32 {
+    match c {
+        Some(')') => 3,
+        Some(']') => 57,
+        Some('}') => 1197,
+        Some('>') => 25137,
+        _ => 0,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_matching_paren() {
+        assert!(is_matching_paren("()"));
+        assert!(is_matching_paren("([{<>}])"));
+        assert!(!is_matching_paren("(]"));
+    }
+
+    #[test]
+    fn test_solve1() {
+        const TEST_DATA: &str = "
+            [({(<(())[]>[[{[]{<()<>>
+            [(()[<>])]({[<{<<[]>>(
+            {([(<{}[<>[]}>{[]{[(<()>
+            (((({<>}<{<{<>}{[]{[]{}
+            [[<[([]))<([[{}[[()]]]
+            [{[{({}]{}}([{[{{{}}([]
+            {<[[]]>}<{[{[{[]{()[[[]
+            [<(<(<(<{}))><([]([]()
+            <{([([[(<>()){}]>(<<{{
+            <{([{{}}[<[[[<>{}]]]>[]]
+        ";
+        assert_eq!(solve1(TEST_DATA), 26397);
+    }
+}
